c26176ab4a97f4d6f1be07d497ad47c27dbcdc6a
Atsushi <atu729@icloud.com>
2024-08-14 00:56:11 +0900

    call db connection setting

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const application = express();
+const connect = require("./db");
+
+connect();
 
 application.listen(3000, () => {
   console.log("Server is running on port 3000");
