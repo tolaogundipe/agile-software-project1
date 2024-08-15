da72d1196e1426e4ae10f06233198a303f328fa8
Atsushi <atu729@icloud.com>
2024-08-15 21:55:22 +0900

    add recipes routing

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const application = express();
 const connect = require("./db");
+const Recipe = require("./models/recipe");
+const cors = require("cors");
+require("dotenv").config(); // Load environment variables
 
+// Connect to the database
 connect();
 
-application.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Middleware
+application.use(express.json());
+application.use(cors());
+
+// Routes
+application.get("/recipes", async (request, response) => {
+  try {
+    const recipes = await Recipe.find({});
+    return response.json({ recipes });
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    return response.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+application.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
