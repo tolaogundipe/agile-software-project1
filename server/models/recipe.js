0b543781739e0903aab7e1ec94433511b68a0643
Atsushi <atu729@icloud.com>
2024-08-15 21:51:01 +0900

    create recipe model

---
+++ server/models/recipe.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: Number,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+  },
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+module.exports = Recipe;
